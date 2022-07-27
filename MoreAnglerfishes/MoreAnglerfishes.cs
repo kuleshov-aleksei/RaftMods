9d7d62b2d5c7c58d2525f8104edf391454287366
kuleshov-aleksei <alexxey67@gmail.com>
2022-07-27 22:11:26 +0300

    More routes, destroy AI on landmark reset

--- MoreAnglerfishes/MoreAnglerfishes.cs
+++ MoreAnglerfishes/MoreAnglerfishes.cs
@@ -4,114 +4,105 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class MoreAnglerfishes : Mod
+namespace MoreAnglerfishes
 {
-    private Harmony m_harmony;
-    public void Start()
+    public class MoreAnglerfishes : Mod
     {
-        Debug.Log("Mod MoreAnglerfishes has been loaded!");
-        m_harmony = new Harmony("com.encamy.more-anglerfishes");
-        m_harmony.PatchAll();
-    }
-
-    public void OnModUnload()
-    {
-        m_harmony.UnpatchAll(m_harmony.Id);
-        Debug.Log("Mod MoreAnglerfishes has been unloaded!");
-    }
-
-    [ConsoleCommand(name: "getPos", docs: "getPos")]
-    public static void GetPosition()
-    {
-        Raft_Network raftNetwork = ComponentManager<Raft_Network>.Value;
-        Vector3 position = raftNetwork.GetLocalPlayer().FeetPosition;
-        Debug.Log($"Position is {position.x} {position.y} {position.z}");
-    }
-}
-
-[HarmonyPatch(typeof(LandmarkEntitySpawner), "CreateEntity")]
-public class LandmarkEntitySpawner_CreateEntity_Patch
-{
-    // Exclusive upper bound
-    private const int MAXIMUM_ANGLERFISHES_PER_LANDMARK = 4;
-    private const int MINUMUM_ANGLERFISHES_PER_LANDMARK = 1;
-    private const int MAX_AVAILABLE_LOCATIONS = 10;
-    private const int RANDOM_SEED = 42069;
-    private static DateTime m_lastSpawnTime = default;
-    private static TimeSpan m_throttleDuration = TimeSpan.FromSeconds(5);
-
-    private static readonly List<string> m_allowedSpawnPoints = new List<string>
-    {
-        "Item/Stone",
-        "Item/Sand",
-        "Item/Scrap",
-        "Item/Clay",
-        "Item/GiantClam",
-    };
-
-    public static void Postfix(ref LandmarkEntitySpawner __instance)
-    {
-        Landmark landmark = __instance.landmark;
-        Debug.Log($"Landmark {landmark.name} spawned");
-
-        // Prevent spawning multiple anglerfishes at the same location
-        if (DateTime.UtcNow <= m_lastSpawnTime + m_throttleDuration)
+        private Harmony m_harmony;
+        public void Start()
         {
-            return;
+            Debug.Log("Mod MoreAnglerfishes has been loaded!");
+            m_harmony = new Harmony("com.encamy.more-anglerfishes");
+            m_harmony.PatchAll();
         }
 
-        m_lastSpawnTime = DateTime.UtcNow;
-
-        if (!Raft_Network.IsHost)
+        public void OnModUnload()
         {
-            return;
+            m_harmony.UnpatchAll(m_harmony.Id);
+            Debug.Log("Mod MoreAnglerfishes has been unloaded!");
         }
 
-        System.Random random = new System.Random(RANDOM_SEED);
-        IEnumerable<PickupItem> items = landmark.GetComponentsInChildren<PickupItem>()
-            .Where(x => m_allowedSpawnPoints.Contains(x.pickupTerm))
-            .OrderBy(x => x.transform.position.y) // deepest objects go first
-            .Take(MAX_AVAILABLE_LOCATIONS) // narrow pool of spawnable location
-            .OrderBy(x => random.Next()) // shuffle pool
-            .Take(random.Next(MINUMUM_ANGLERFISHES_PER_LANDMARK, MAXIMUM_ANGLERFISHES_PER_LANDMARK)); // take N final locations
-
-        foreach (PickupItem pickupItem in items)
+        [ConsoleCommand(name: "getPos", docs: "getPos")]
+        public static void GetPosition()
         {
-            Vector3 position = pickupItem.transform.position;
-            Quaternion quaternion = pickupItem.transform.rotation;
+            Raft_Network raftNetwork = ComponentManager<Raft_Network>.Value;
+            Vector3 position = raftNetwork.GetLocalPlayer().FeetPosition;
+            Debug.Log($"Position is {position.x} {position.y} {position.z}");
+        }
+    }
 
-            GameObject gameObject = new GameObject();
-            LandmarkEntitySpawner anglerFishSpawner = gameObject.AddComponent<LandmarkEntitySpawner>();
-            WaypointHandler waypointHandler = gameObject.AddComponent<WaypointHandler>();
-            Waypoint headWaypoint = gameObject.AddComponent<Waypoint>();
-            anglerFishSpawner.waypointHandler = waypointHandler;
-            anglerFishSpawner.spawnerIndex = 420;
-            anglerFishSpawner.landmark = landmark;
-            headWaypoint.waypointHandler = waypointHandler;
+    [HarmonyPatch(typeof(Landmark), "OnSpawn")]
+    public class Landmark_CreateEntity_Patch
+    {
+        // Exclusive upper bound
+        private const int MAXIMUM_ANGLERFISHES_PER_LANDMARK = 6;
+        private const int MINUMUM_ANGLERFISHES_PER_LANDMARK = 4;
+        private const int MAX_AVAILABLE_LOCATIONS = 20;
+        private const int RANDOM_SEED = 42069;
 
-            position.y += random.Next(3, 5);
+        private static readonly List<string> m_allowedSpawnPoints = new List<string>
+        {
+            "Item/Stone",
+            "Item/Sand",
+            "Item/Scrap",
+            "Item/Clay",
+            "Item/GiantClam",
+        };
 
-            position.x -= random.Next(1, 4);
-            Waypoint firstWaypoint = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
-            position.z -= random.Next(1, 4);
-            Waypoint secondWaypoint = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
-            position.x += random.Next(1, 4);
-            Waypoint thirdWaypoint = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
-            position.z += random.Next(1, 4);
-            Waypoint forthWaypoint = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
-            firstWaypoint.next = secondWaypoint;
-            secondWaypoint.next = thirdWaypoint;
-            thirdWaypoint.next = forthWaypoint;
-            forthWaypoint.next = firstWaypoint;
+        private static readonly Queue<uint> m_killList = new Queue<uint>();
 
-            waypointHandler.waypoints.Add(firstWaypoint);
-            waypointHandler.waypoints.Add(secondWaypoint);
-            waypointHandler.waypoints.Add(thirdWaypoint);
-            waypointHandler.waypoints.Add(forthWaypoint);
+        public static void Postfix(ref Landmark __instance)
+        {
+            Landmark landmark = __instance;
+            Debug.Log($"Landmark {landmark.name} spawned");
 
-            Debug.Log($"Spawning Anglerfish at {position.x} {position.y} {position.z}");
+            landmark.OnLandmarkReset += LandmarkReset;
 
-            ComponentManager<Network_Host_Entities>.Value.CreateAINetworkBehaviour(AI_NetworkBehaviourType.AnglerFish, position, anglerFishSpawner);
+            if (!Raft_Network.IsHost)
+            {
+                return;
+            }
+
+            System.Random random = new System.Random(RANDOM_SEED);
+            IEnumerable<PickupItem> items = landmark.GetComponentsInChildren<PickupItem>()
+                .Where(x => m_allowedSpawnPoints.Contains(x.pickupTerm))
+                .OrderBy(x => x.transform.position.y) // deepest objects go first
+                .Take(MAX_AVAILABLE_LOCATIONS) // narrow pool of spawnable location
+                .OrderBy(x => random.Next()) // shuffle pool
+                .Take(random.Next(MINUMUM_ANGLERFISHES_PER_LANDMARK, MAXIMUM_ANGLERFISHES_PER_LANDMARK)); // take N final locations
+
+            foreach (PickupItem pickupItem in items)
+            {
+                GameObject gameObject = new GameObject();
+                LandmarkEntitySpawner anglerFishSpawner = gameObject.AddComponent<LandmarkEntitySpawner>();
+                WaypointHandler waypointHandler = gameObject.AddComponent<WaypointHandler>();
+                Waypoint headWaypoint = gameObject.AddComponent<Waypoint>();
+                anglerFishSpawner.waypointHandler = waypointHandler;
+                anglerFishSpawner.spawnerIndex = 420;
+                anglerFishSpawner.landmark = landmark;
+                headWaypoint.waypointHandler = waypointHandler;
+
+                Vector3 position = pickupItem.transform.position;
+                Quaternion quaternion = pickupItem.transform.rotation;
+                position.y += random.Next(3, 5);
+
+                WaypointFactory.CreateWaypoints(waypointHandler, position, random, headWaypoint, quaternion);
+
+                Debug.Log($"Spawning Anglerfish at {position.x} {position.y} {position.z}");
+
+                AI_NetworkBehaviour aiBehaviour = ComponentManager<Network_Host_Entities>.Value.CreateAINetworkBehaviour(AI_NetworkBehaviourType.AnglerFish, position, anglerFishSpawner);
+                m_killList.Enqueue(aiBehaviour.ObjectIndex);
+            }
+        }
+
+        private static void LandmarkReset()
+        {
+            Debug.Log("Landmark reset, clearing stale anglerfishes");
+            while (m_killList.Count > 0)
+            {
+                uint objectToKillIndex = m_killList.Dequeue();
+                NetworkIDManager.SendIDBehaviourDead(objectToKillIndex, typeof(AI_NetworkBehaviour), true);
+            }
         }
     }
 }
---
+++ MoreAnglerfishes/WaypointFactory.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoreAnglerfishes
+{
+    internal static class WaypointFactory
+    {
+        private static int m_currentPathTypeId = 0;
+
+        public static void CreateWaypoints(
+            WaypointHandler waypointHandler,
+            Vector3 initalPosition,
+            System.Random random,
+            Waypoint headWaypoint,
+            Quaternion quaternion)
+        {
+            m_currentPathTypeId++;
+            m_currentPathTypeId %= m_pathsElementsCount;
+            PathType pathType = (PathType)m_currentPathTypeId;
+            Debug.Log($"Current path type id is {m_currentPathTypeId} {pathType}");
+
+            List<Waypoint> waypoints;
+            switch (pathType)
+            {
+                case PathType.MRoute:
+                    waypoints = CreateMRoute(initalPosition, random, headWaypoint, quaternion);
+                    break;
+                case PathType.Star:
+                    waypoints = CreateStarRoute(initalPosition, random, headWaypoint, quaternion);
+                    break;
+                case PathType.Circle:
+                default:
+                    waypoints = CreateCircleRoute(initalPosition, random, headWaypoint, quaternion);
+                    break;
+            }
+
+            waypointHandler.waypoints.AddRange(waypoints);
+        }
+
+        private static List<Waypoint> CreateMRoute(Vector3 initalPosition, System.Random random, Waypoint headWaypoint, Quaternion quaternion)
+        {
+            Debug.Log("Using M-route");
+            Vector3 position = initalPosition;
+
+            position.z -= 4;
+            Waypoint waypointA = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            position.x -= 4;
+            position.z += 2;
+            Waypoint waypointB = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            position.z += 2;
+            position.x += 4;
+            Waypoint waypointC = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            position.x -= 4;
+            position.z += 2;
+            Waypoint waypointD = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            position.z += 2;
+            position.x += 4;
+            Waypoint waypointE = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            waypointA.next = waypointB;
+            waypointB.next = waypointC;
+            waypointC.next = waypointD;
+            waypointD.next = waypointE;
+            waypointE.next = waypointA;
+
+            List<Waypoint> resultWaypoints = new List<Waypoint>(5);
+            resultWaypoints.Add(waypointA);
+            resultWaypoints.Add(waypointB);
+            resultWaypoints.Add(waypointC);
+            resultWaypoints.Add(waypointD);
+            resultWaypoints.Add(waypointE);
+            return resultWaypoints;
+        }
+
+        private static List<Waypoint> CreateStarRoute(Vector3 initalPosition, System.Random random, Waypoint headWaypoint, Quaternion quaternion)
+        {
+            Debug.Log("Using star route");
+            Vector3 position = initalPosition;
+
+            position.x -= 4;
+            Waypoint waypointA = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            position.z += 2;
+            position.x += 6;
+            Waypoint waypointB = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            position.z -= 4;
+            position.x -= 4;
+            Waypoint waypointC = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            position.z += 4;
+            Waypoint waypointD = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            position.z -= 4;
+            position.x += 4;
+            Waypoint waypointE = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+
+            waypointA.next = waypointB;
+            waypointB.next = waypointC;
+            waypointC.next = waypointD;
+            waypointD.next = waypointE;
+            waypointE.next = waypointA;
+
+            List<Waypoint> resultWaypoints = new List<Waypoint>(5);
+            resultWaypoints.Add(waypointA);
+            resultWaypoints.Add(waypointB);
+            resultWaypoints.Add(waypointC);
+            resultWaypoints.Add(waypointD);
+            resultWaypoints.Add(waypointE);
+
+            return resultWaypoints;
+        }
+
+        private static List<Waypoint> CreateCircleRoute(Vector3 initalPosition, System.Random random, Waypoint headWaypoint, Quaternion quaternion)
+        {
+            Debug.Log("Using circle route");
+            Vector3 position = initalPosition;
+
+            position.x -= random.Next(1, 4);
+            Waypoint firstWaypoint = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+            position.z -= random.Next(1, 4);
+            Waypoint secondWaypoint = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+            position.x += random.Next(1, 4);
+            Waypoint thirdWaypoint = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+            position.z += random.Next(1, 4);
+            Waypoint forthWaypoint = GameObject.Instantiate<Waypoint>(headWaypoint, position, quaternion);
+            firstWaypoint.next = secondWaypoint;
+            secondWaypoint.next = thirdWaypoint;
+            thirdWaypoint.next = forthWaypoint;
+            forthWaypoint.next = firstWaypoint;
+
+            List<Waypoint> resultWaypoints = new List<Waypoint>(4);
+            resultWaypoints.Add(firstWaypoint);
+            resultWaypoints.Add(secondWaypoint);
+            resultWaypoints.Add(thirdWaypoint);
+            resultWaypoints.Add(forthWaypoint);
+            return resultWaypoints;
+        }
+
+        // hardcode for faster runtime
+        private static readonly int m_pathsElementsCount = 3;
+        public enum PathType
+        {
+            Circle = 0,
+            MRoute,
+            Star,
+        }
+    }
+}
